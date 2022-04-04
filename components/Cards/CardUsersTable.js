ebefa6a1157bda645598e1a2ea07594f945ed535
khanhpham-29 <84856181+khanhpham-29@users.noreply.github.com>
2022-04-04 23:06:54 +0700

    add-user-list

---
+++ components/Cards/CardUsersTable.js
@@ -0,0 +1,178 @@
+import React from "react";
+import PropTypes from "prop-types";
+
+// components
+
+import TableDropdown from "components/Dropdowns/TableDropdown.js";
+import { useRouter } from "next/router";
+
+const pageList = [];
+const NumPage = 10;
+
+function createPageList(NumPage) {
+  for (let i = 0; i < NumPage; i++) {
+    pageList.push(i + 1);
+  }
+}
+
+
+export default function CardUsersTable({ color, title, Users }) {
+  const router = useRouter();
+  function handleAdd() {
+    router.push(router.route + "/add");
+  }
+
+  createPageList(NumPage);
+  return (
+    <>
+      <div
+        className={
+          "relative flex flex-col min-w-0 break-words w-full mb-6 shadow-lg rounded " +
+          (color === "light" ? "bg-white" : "bg-blueGray-700 text-white")
+        }
+      >
+        <div className="rounded-t mb-0 px-4 py-3 border-0">
+          <div className="flex flex-wrap items-center">
+            <div className="relative w-full px-4 max-w-full flex-grow flex-1">
+              <h3
+                className={
+                  "font-semibold text-lg " +
+                  (color === "light" ? "text-blueGray-700" : "text-white")
+                }
+              >
+                {title ?? "Card Tables"}
+              </h3>
+              <button
+                className="bg-blueGray-700 active:bg-blueGray-600 text-white font-bold uppercase text-xs px-4 py-2 rounded shadow hover:shadow-md outline-none focus:outline-none mr-1 ease-linear transition-all duration-150"
+                type="button"
+                onClick={() => handleAdd()}
+              >
+                Add
+              </button>
+            </div>
+          </div>
+        </div>
+        <div className="block w-full overflow-x-auto">
+          {/* Projects table */}
+          <table className="items-center w-full bg-transparent">
+            <thead>
+              <tr>
+                <th
+                  className={
+                    "px-6 align-middle border border-solid py-3 text-xs uppercase border-l-0 border-r-0 whitespace-nowrap font-semibold text-left " +
+                    (color === "light"
+                      ? "bg-blueGray-50 text-blueGray-500 border-blueGray-100"
+                      : "bg-blueGray-600 text-blueGray-200 border-blueGray-500")
+                  }
+                >
+                  User Name
+                </th>
+                <th
+                  className={
+                    "px-6 align-middle border border-solid py-3 text-xs uppercase border-l-0 border-r-0 whitespace-nowrap font-semibold text-left " +
+                    (color === "light"
+                      ? "bg-blueGray-50 text-blueGray-500 border-blueGray-100"
+                      : "bg-blueGray-600 text-blueGray-200 border-blueGray-500")
+                  }
+                >
+                  Email
+                </th>
+                <th
+                  className={
+                    "px-6 align-middle border border-solid py-3 text-xs uppercase border-l-0 border-r-0 whitespace-nowrap font-semibold text-left " +
+                    (color === "light"
+                      ? "bg-blueGray-50 text-blueGray-500 border-blueGray-100"
+                      : "bg-blueGray-600 text-blueGray-200 border-blueGray-500")
+                  }
+                >
+                  First Name
+                </th>
+                <th
+                  className={
+                    "px-6 align-middle border border-solid py-3 text-xs uppercase border-l-0 border-r-0 whitespace-nowrap font-semibold text-left " +
+                    (color === "light"
+                      ? "bg-blueGray-50 text-blueGray-500 border-blueGray-100"
+                      : "bg-blueGray-600 text-blueGray-200 border-blueGray-500")
+                  }
+                >
+                  Last Name
+                </th>
+                <th
+                  className={
+                    "px-6 align-middle border border-solid py-3 text-xs uppercase border-l-0 border-r-0 whitespace-nowrap font-semibold text-left " +
+                    (color === "light"
+                      ? "bg-blueGray-50 text-blueGray-500 border-blueGray-100"
+                      : "bg-blueGray-600 text-blueGray-200 border-blueGray-500")
+                  }
+                >
+                  Address
+                </th>
+                <th
+                  className={
+                    "px-6 align-middle border border-solid py-3 text-xs uppercase border-l-0 border-r-0 whitespace-nowrap font-semibold text-left " +
+                    (color === "light"
+                      ? "bg-blueGray-50 text-blueGray-500 border-blueGray-100"
+                      : "bg-blueGray-600 text-blueGray-200 border-blueGray-500")
+                  }
+                ></th>
+              </tr>
+            </thead>
+
+            {/* table data */}
+
+            <tbody>
+              {
+                Users.map(User => (
+                  <tr>
+                    <td className="border-t-0 px-6 align-middle border-l-0 border-r-0 text-xs whitespace-nowrap p-4">
+                      {User.fullName}
+                    </td>
+                    <td className="border-t-0 px-6 align-middle border-l-0 border-r-0 text-xs whitespace-nowrap p-4">
+                      {User.email}
+                    </td>
+                    <td className="border-t-0 px-6 align-middle border-l-0 border-r-0 text-xs whitespace-nowrap p-4">
+                      {User.firstName}
+                    </td>
+                    <td className="border-t-0 px-6 align-middle border-l-0 border-r-0 text-xs whitespace-nowrap p-4">
+                      {User.lastName}
+                    </td>
+                    <td className="border-t-0 px-6 align-middle border-l-0 border-r-0 text-xs whitespace-nowrap p-4">
+                      {User.address}
+                    </td>
+                    <td className="border-t-0 px-6 align-middle border-l-0 border-r-0 text-xs whitespace-nowrap p-4 text-right">
+                      <TableDropdown />
+                    </td>
+                  </tr>
+                ))
+              }
+            </tbody>
+          </table>
+        </div>
+      </div>
+
+      <div class="container flex justify-center mx-auto">
+        <ul class="flex">
+          <li><button class="px-4 py-2 text-gray-600 bg-white border border-gray-600">Prev</button>
+          </li>
+
+          {
+            pageList.map(page =>
+              <li><button class="px-4 py-2 text-gray-600 bg-white border border-gray-600 ">{page}</button>
+              </li>
+            )
+          }
+          <li><button class="px-4 py-2 text-gray-600 bg-white border border-gray-600">Next</button>
+          </li>
+        </ul>
+      </div>
+    </>
+  );
+}
+
+CardUsersTable.defaultProps = {
+  color: "light",
+};
+
+CardUsersTable.propTypes = {
+  color: PropTypes.oneOf(["light", "dark"]),
+};
--- pages/admin/users/index.js
+++ pages/admin/users/index.js
@@ -2,7 +2,7 @@ import React from "react";
 
 // components
 
-import CardTable from "components/Cards/CardTable.js";
+import CardUsersTable from "components/Cards/CardUsersTable.js";
 
 // layout for page
 
@@ -13,10 +13,10 @@ export default function Index() {
     <>
       <div className="flex flex-wrap mt-4">
         <div className="w-full mb-12 px-4">
-          <CardTable title="Users" />
+          <CardUsersTable title="Users"/>
         </div>
         {/* <div className="w-full mb-12 px-4">
-          <CardTable color="dark" title="Users" />
+          <CardUsersTable color="dark" title="Users" />
         </div> */}
       </div>
     </>
