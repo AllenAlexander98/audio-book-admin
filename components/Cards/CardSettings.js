c1522c4c404e6c2b0b191c24baa28573ab431d9a
nhanlinh1478 <luongtrongnhan1082000@gmail.com>
2022-04-05 00:17:38 +0700

    30-edit-category-audio-book-admin-and-edit-card-add-category

--- components/Cards/CardSettings.js
+++ components/Cards/CardSettings.js
@@ -1,27 +1,43 @@
-import React from "react";
-
+import React, { useState } from "react";
+import { toast } from "react-toastify";
 // components
 
 export default function CardSettings({ title }) {
+  const [name, setName] = useState();
+  const handleChangeTile = (e) => {
+    setName(e.target.value);
+    console.log("title:", name);
+  };
+  const notify = () => {
+    toast.success("Success Notification !", {
+      position: toast.POSITION.TOP_CENTER,
+    });
+  };
+  const handleSubmit = () => {
+    //   //call API to submit name category at here
+    //   // then update data with table category
+    console.log("name category:", name);
+  };
   return (
     <>
-      <div className="relative flex flex-col min-w-0 break-words w-full mb-6 shadow-lg rounded-lg bg-blueGray-100 border-0">
-        <div className="rounded-t bg-white mb-0 px-6 py-6">
-          <div className="text-center flex justify-between">
-            <h6 className="text-blueGray-700 text-xl font-bold">
-              {title ?? "My account"}
-            </h6>
-            <button
-              className="bg-blueGray-700 active:bg-blueGray-600 text-white font-bold uppercase text-xs px-4 py-2 rounded shadow hover:shadow-md outline-none focus:outline-none mr-1 ease-linear transition-all duration-150"
-              type="button"
-            >
-              Settings
-            </button>
+      <form method="POST" onSubmit={handleSubmit}>
+        <div className="relative flex flex-col min-w-0 break-words w-full mb-6 shadow-lg rounded-lg bg-blueGray-100 border-0">
+          <div className="rounded-t bg-white mb-0 px-6 py-6">
+            <div className="text-center flex justify-between">
+              <h6 className="text-blueGray-700 text-xl font-bold">
+                {title ?? "My account"}
+              </h6>
+              <button
+                className="bg-blueGray-700 active:bg-blueGray-600 text-white font-bold uppercase text-xs px-4 py-2 rounded shadow hover:shadow-md outline-none focus:outline-none mr-1 ease-linear transition-all duration-150"
+                type="submit"
+                onClick={notify}
+              >
+                save
+              </button>
+            </div>
           </div>
-        </div>
-        <div className="flex-auto px-4 lg:px-10 py-10 pt-0">
-          <form>
-            <h6 className="text-blueGray-400 text-sm mt-3 mb-6 font-bold uppercase">
+          <div className="flex-auto px-4 lg:px-10 py-10 pt-0">
+            {/* <h6 className="text-blueGray-400 text-sm mt-3 mb-6 font-bold uppercase">
               User Information
             </h6>
             <div className="flex flex-wrap">
@@ -85,12 +101,12 @@ export default function CardSettings({ title }) {
                   />
                 </div>
               </div>
-            </div>
+            </div> */}
 
             <hr className="mt-6 border-b-1 border-blueGray-300" />
 
             <h6 className="text-blueGray-400 text-sm mt-3 mb-6 font-bold uppercase">
-              Contact Information
+              Category Information
             </h6>
             <div className="flex flex-wrap">
               <div className="w-full lg:w-12/12 px-4">
@@ -99,18 +115,18 @@ export default function CardSettings({ title }) {
                     className="block uppercase text-blueGray-600 text-xs font-bold mb-2"
                     htmlFor="grid-password"
                   >
-                    Address
+                    title
                   </label>
                   <input
                     type="text"
                     className="border-0 px-3 py-3 placeholder-blueGray-300 text-blueGray-600 bg-white rounded text-sm shadow focus:outline-none focus:ring w-full ease-linear transition-all duration-150"
-                    defaultValue="Bld Mihail Kogalniceanu, nr. 8 Bl 1, Sc 1, Ap 09"
+                    onChange={handleChangeTile}
                   />
                 </div>
               </div>
               <div className="w-full lg:w-4/12 px-4">
                 <div className="relative w-full mb-3">
-                  <label
+                  {/* <label
                     className="block uppercase text-blueGray-600 text-xs font-bold mb-2"
                     htmlFor="grid-password"
                   >
@@ -150,14 +166,14 @@ export default function CardSettings({ title }) {
                     type="text"
                     className="border-0 px-3 py-3 placeholder-blueGray-300 text-blueGray-600 bg-white rounded text-sm shadow focus:outline-none focus:ring w-full ease-linear transition-all duration-150"
                     defaultValue="Postal Code"
-                  />
+                  /> */}
                 </div>
               </div>
             </div>
 
             <hr className="mt-6 border-b-1 border-blueGray-300" />
 
-            <h6 className="text-blueGray-400 text-sm mt-3 mb-6 font-bold uppercase">
+            {/* <h6 className="text-blueGray-400 text-sm mt-3 mb-6 font-bold uppercase">
               About Me
             </h6>
             <div className="flex flex-wrap">
@@ -178,10 +194,10 @@ export default function CardSettings({ title }) {
                   ></textarea>
                 </div>
               </div>
-            </div>
-          </form>
+            </div> */}
+          </div>
         </div>
-      </div>
+      </form>
     </>
   );
 }
--- pages/admin/categories/index.js
+++ pages/admin/categories/index.js
@@ -1,9 +1,10 @@
 import React from "react";
+import { format, formatDistance, formatRelative, subDays } from "date-fns";
 
 // components
 
 import CategoryCardTable from "components/Cards/CategoryCardTable.js";
-
+import CategoryCardTableInfor from "components/Cards/CategoryCardTableInInfor";
 // layout for page
 
 import Admin from "layouts/Admin.js";
@@ -12,10 +13,56 @@ const categories = [
   {
     id: 1,
     name: "Category 1",
+    status: "pending",
+    user: {
+      name: "david",
+      date: formatRelative(subDays(new Date(), 3), new Date()),
+    },
   },
   {
     id: 2,
     name: "Category 2",
+    status: "active",
+    user: {
+      name: "david",
+      date: formatRelative(subDays(new Date(), 3), new Date()),
+    },
+  },
+  {
+    id: 3,
+    name: "Category 1",
+    status: "pending",
+    user: {
+      name: "david",
+      date: formatRelative(subDays(new Date(), 3), new Date()),
+    },
+  },
+  {
+    id: 4,
+    name: "Category 2",
+    status: "active",
+    user: {
+      name: "david",
+      date: formatRelative(subDays(new Date(), 3), new Date()),
+    },
+  },
+  {
+    id: 5,
+    name: "Category 1",
+    status: "pending",
+    user: {
+      name: "david",
+      date: formatRelative(subDays(new Date(), 3), new Date()),
+    },
+  },
+  {
+    id: 6,
+    name: "Category 2",
+    status: "active",
+    user: {
+      name: "david",
+      date: formatRelative(subDays(new Date(), 3), new Date()),
+    },
   },
 ];
 
@@ -24,7 +71,7 @@ export default function Index() {
     <>
       <div className="flex flex-wrap mt-4">
         <div className="w-full mb-12 px-4">
-          <CategoryCardTable title="Caretories" categories={categories} />
+          <CategoryCardTableInfor title="Categories" categories={categories} />
         </div>
         {/* <div className="w-full mb-12 px-4">
           <CategoryCardTable color="dark" title="Caretories" />
