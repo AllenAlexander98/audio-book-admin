6bdc235eab7d9c8d185cdb8105f750f6ab856c4a
Tuan Nguyen <ntuan.2502@gmail.com>
2022-04-05 09:47:44 +0700

    release 28-danh-sách-người-dùng-admin

--- components/Cards/CardUsersTable.js
+++ components/Cards/CardUsersTable.js
@@ -1,46 +1,167 @@
-import React from "react";
 import PropTypes from "prop-types";
-
-// components
-
 import TableDropdown from "components/Dropdowns/TableDropdown.js";
 import { useRouter } from "next/router";
+import { useEffect } from "react";
 
-const users = [];
-
-function fakedata() {
-  var firstNames = ['Pilar', 'Kentrell', 'Octavio', 'Jade', 'Niya', 'Perry', 'Ryleigh', 'Wayne', 'Cedrick', 'Daron'];
-  var lastNames = ['Brice', 'Jairo', 'Christopher', 'Mattie', 'Belen', 'Jessie', 'Albert', 'Talon', 'Mckenzie', 'Konnor'];
-  var streets = ['Democracy', 'Department', 'Dimitri', 'Dock', 'Dubni', 'Easter', 'East Hills', 'Easy', 'Elgin', 'Elisabeth'];
-  var districts = ['New York City, NY', 'Los Angeles, CA', 'Chicago, IL', 'Houston, TX', 'Phoenix, AZ', 'Philadelphia, PA', 'San Antonio, TX', 'San Diego, CA', 'Dallas, TX', 'San Jose, CA'];
-  var states = ['New Jersey', 'New Mexico', 'New York', 'North Carolina', 'North Dakota', 'Ohio', 'Oklahoma', 'Oregon', 'PennsylvaniaRhode Island', 'South Carolina'];
+const users = [
+  {
+    firstName: "Wayne",
+    lastName: "Albert",
+    fullName: "Wayne Albert",
+    email: "WayneAlbert@gmail.com",
+    address: "234, Dimitri, San Diego, CA, New Jersey",
+  },
+  {
+    firstName: "Perry",
+    lastName: "Mattie",
+    fullName: "Perry Mattie",
+    email: "PerryMattie@gmail.com",
+    address: "224, Dimitri, San Jose, CA, Ohio",
+  },
+  {
+    firstName: "Niya",
+    lastName: "Mattie",
+    fullName: "Niya Mattie",
+    email: "NiyaMattie@gmail.com",
+    address: "713, Dimitri, New York City, NY, South Carolina",
+  },
+  {
+    firstName: "Jade",
+    lastName: "Konnor",
+    fullName: "Jade Konnor",
+    email: "JadeKonnor@gmail.com",
+    address: "787, Easy, Dallas, TX, North Dakota",
+  },
+  {
+    firstName: "Daron",
+    lastName: "Mckenzie",
+    fullName: "Daron Mckenzie",
+    email: "DaronMckenzie@gmail.com",
+    address: "167, Easter, New York City, NY, New Jersey",
+  },
+  {
+    firstName: "Ryleigh",
+    lastName: "Talon",
+    fullName: "Ryleigh Talon",
+    email: "RyleighTalon@gmail.com",
+    address: "304, Elgin, New York City, NY, North Carolina",
+  },
+  {
+    firstName: "Ryleigh",
+    lastName: "Albert",
+    fullName: "Ryleigh Albert",
+    email: "RyleighAlbert@gmail.com",
+    address: "818, Department, Dallas, TX, Oregon",
+  },
+  {
+    firstName: "Kentrell",
+    lastName: "Belen",
+    fullName: "Kentrell Belen",
+    email: "KentrellBelen@gmail.com",
+    address: "149, Dimitri, San Antonio, TX, North Carolina",
+  },
+  {
+    firstName: "Cedrick",
+    lastName: "Talon",
+    fullName: "Cedrick Talon",
+    email: "CedrickTalon@gmail.com",
+    address: "576, Dock, San Diego, CA, PennsylvaniaRhode Island",
+  },
+  {
+    firstName: "Wayne",
+    lastName: "Jessie",
+    fullName: "Wayne Jessie",
+    email: "WayneJessie@gmail.com",
+    address: "215, Easy, San Jose, CA, South Carolina",
+  },
+];
+const pageList = [];
+const numPage = 10;
+function fakeData() {
+  var firstNames = [
+    "Pilar",
+    "Kentrell",
+    "Octavio",
+    "Jade",
+    "Niya",
+    "Perry",
+    "Ryleigh",
+    "Wayne",
+    "Cedrick",
+    "Daron",
+  ];
+  var lastNames = [
+    "Brice",
+    "Jairo",
+    "Christopher",
+    "Mattie",
+    "Belen",
+    "Jessie",
+    "Albert",
+    "Talon",
+    "Mckenzie",
+    "Konnor",
+  ];
+  var streets = [
+    "Democracy",
+    "Department",
+    "Dimitri",
+    "Dock",
+    "Dubni",
+    "Easter",
+    "East Hills",
+    "Easy",
+    "Elgin",
+    "Elisabeth",
+  ];
+  var districts = [
+    "New York City, NY",
+    "Los Angeles, CA",
+    "Chicago, IL",
+    "Houston, TX",
+    "Phoenix, AZ",
+    "Philadelphia, PA",
+    "San Antonio, TX",
+    "San Diego, CA",
+    "Dallas, TX",
+    "San Jose, CA",
+  ];
+  var states = [
+    "New Jersey",
+    "New Mexico",
+    "New York",
+    "North Carolina",
+    "North Dakota",
+    "Ohio",
+    "Oklahoma",
+    "Oregon",
+    "PennsylvaniaRhode Island",
+    "South Carolina",
+  ];
 
   for (let i = 0; i < 10; i++) {
-    var firstName = firstNames[(Math.floor(Math.random() * 10))];
-    var lastName = lastNames[(Math.floor(Math.random() * 10))];
-    var street = streets[(Math.floor(Math.random() * 10))];
-    var district = districts[(Math.floor(Math.random() * 10))];
-    var state = states[(Math.floor(Math.random() * 10))];
+    var firstName = firstNames[Math.floor(Math.random() * 10)];
+    var lastName = lastNames[Math.floor(Math.random() * 10)];
+    var street = streets[Math.floor(Math.random() * 10)];
+    var district = districts[Math.floor(Math.random() * 10)];
+    var state = states[Math.floor(Math.random() * 10)];
     var no = Math.floor(Math.random() * 1000);
 
-    var fullName = firstName + ' ' + lastName;
-    var email = firstName + lastName + '@gmail.com';
-    var address = no + ', ' + street + ', ' + district + ', ' + state;
+    var fullName = firstName + " " + lastName;
+    var email = firstName + lastName + "@gmail.com";
+    var address = no + ", " + street + ", " + district + ", " + state;
 
     var user = {
-      'firstName': firstName,
-      'lastName': lastName,
-      'fullName': fullName,
-      'email': email,
-      'address': address
-    }
+      firstName: firstName,
+      lastName: lastName,
+      fullName: fullName,
+      email: email,
+      address: address,
+    };
 
     users.push(user);
   }
 }
-
-const pageList = [];
-const numPage = 10;
 
 function createPageList(numPage) {
   for (let i = 0; i < numPage; i++) {
@@ -48,13 +169,8 @@ function createPageList(numPage) {
   }
 }
 
-
-export default function CarduTable({ color, title}) {
+export default function CardUsersTable({ color, title }) {
   const router = useRouter();
-  function handleAdd() {
-    router.push(router.route + "/add");
-  }
-  fakedata();
   createPageList(numPage);
   return (
     <>
@@ -78,7 +194,9 @@ export default function CarduTable({ color, title}) {
               <button
                 className="bg-blueGray-700 active:bg-blueGray-600 text-white font-bold uppercase text-xs px-4 py-2 rounded shadow hover:shadow-md outline-none focus:outline-none mr-1 ease-linear transition-all duration-150"
                 type="button"
-                onClick={() => handleAdd()}
+                onClick={() => {
+                  router.push(router.route + "/add");
+                }}
               >
                 Add
               </button>
@@ -154,47 +272,52 @@ export default function CarduTable({ color, title}) {
             {/* table data */}
 
             <tbody>
-              {
-                users.map(user => (
-                  <tr>
-                    <td className="border-t-0 px-6 align-middle border-l-0 border-r-0 text-xs whitespace-nowrap p-4">
-                      {user.fullName}
-                    </td>
-                    <td className="border-t-0 px-6 align-middle border-l-0 border-r-0 text-xs whitespace-nowrap p-4">
-                      {user.email}
-                    </td>
-                    <td className="border-t-0 px-6 align-middle border-l-0 border-r-0 text-xs whitespace-nowrap p-4">
-                      {user.firstName}
-                    </td>
-                    <td className="border-t-0 px-6 align-middle border-l-0 border-r-0 text-xs whitespace-nowrap p-4">
-                      {user.lastName}
-                    </td>
-                    <td className="border-t-0 px-6 align-middle border-l-0 border-r-0 text-xs whitespace-nowrap p-4">
-                      {user.address}
-                    </td>
-                    <td className="border-t-0 px-6 align-middle border-l-0 border-r-0 text-xs whitespace-nowrap p-4 text-right">
-                      <TableDropdown />
-                    </td>
-                  </tr>
-                ))
-              }
+              {users.map((user, key) => (
+                <tr key={key}>
+                  <td className="border-t-0 px-6 align-middle border-l-0 border-r-0 text-xs whitespace-nowrap p-4">
+                    {user.fullName}
+                  </td>
+                  <td className="border-t-0 px-6 align-middle border-l-0 border-r-0 text-xs whitespace-nowrap p-4">
+                    {user.email}
+                  </td>
+                  <td className="border-t-0 px-6 align-middle border-l-0 border-r-0 text-xs whitespace-nowrap p-4">
+                    {user.firstName}
+                  </td>
+                  <td className="border-t-0 px-6 align-middle border-l-0 border-r-0 text-xs whitespace-nowrap p-4">
+                    {user.lastName}
+                  </td>
+                  <td className="border-t-0 px-6 align-middle border-l-0 border-r-0 text-xs whitespace-nowrap p-4">
+                    {user.address}
+                  </td>
+                  <td className="border-t-0 px-6 align-middle border-l-0 border-r-0 text-xs whitespace-nowrap p-4 text-right">
+                    <TableDropdown />
+                  </td>
+                </tr>
+              ))}
             </tbody>
           </table>
         </div>
       </div>
 
-      <div class="container flex justify-center mx-auto">
-        <ul class="flex">
-          <li><button class="px-4 py-2 text-gray-600 bg-white border border-gray-600">Prev</button>
+      <div className="container flex justify-center mx-auto">
+        <ul className="flex">
+          <li>
+            <button className="px-4 py-2 text-gray-600 bg-white border border-gray-600">
+              Prev
+            </button>
           </li>
 
-          {
-            pageList.map(page =>
-              <li><button class="px-4 py-2 text-gray-600 bg-white border border-gray-600 ">{page}</button>
-              </li>
-            )
-          }
-          <li><button class="px-4 py-2 text-gray-600 bg-white border border-gray-600">Next</button>
+          {pageList.map((page, key) => (
+            <li key={key}>
+              <button className="px-4 py-2 text-gray-600 bg-white border border-gray-600 ">
+                {page}
+              </button>
+            </li>
+          ))}
+          <li>
+            <button className="px-4 py-2 text-gray-600 bg-white border border-gray-600">
+              Next
+            </button>
           </li>
         </ul>
       </div>
@@ -202,10 +325,10 @@ export default function CarduTable({ color, title}) {
   );
 }
 
-CarduTable.defaultProps = {
+CardUsersTable.defaultProps = {
   color: "light",
 };
 
-CarduTable.propTypes = {
+CardUsersTable.propTypes = {
   color: PropTypes.oneOf(["light", "dark"]),
 };
--- pages/admin/users/index.js
+++ pages/admin/users/index.js
@@ -1,11 +1,4 @@
-import React from "react";
-
-// components
-
 import CardUsersTable from "components/Cards/CardUsersTable.js";
-
-// layout for page
-
 import Admin from "layouts/Admin.js";
 
 export default function Index() {
@@ -13,11 +6,8 @@ export default function Index() {
     <>
       <div className="flex flex-wrap mt-4">
         <div className="w-full mb-12 px-4">
-          <CardUsersTable title="Users"/>
+          <CardUsersTable title="Users" />
         </div>
-        {/* <div className="w-full mb-12 px-4">
-          <CardUsersTable color="dark" title="Users" />
-        </div> */}
       </div>
     </>
   );
