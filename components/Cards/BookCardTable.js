6b42eff46c22135ffb4c168e824ee500d1f8b887
duytt3 <duytt3@vng.com.vn>
2022-04-05 11:53:06 +0700

    add BookCardTable.js, add file image book1.jpg

---
+++ components/Cards/BookCardTable.js
@@ -0,0 +1,237 @@
+import React, { useState } from "react";
+import PropTypes from "prop-types";
+import TablePagination from "@mui/material/TablePagination";
+import Button from '@mui/material/Button';
+// components
+
+import TableDropdown from "components/Dropdowns/TableDropdown.js";
+import { useRouter } from "next/router";
+
+export default function BookCardTable({ color, title, books }) {
+  const router = useRouter();
+  const [page, setPage] = useState(0);
+  const [rowsPerPage, setRowsPerPage] = useState(10);
+
+  function handleAdd() {
+    router.push(router.route + "/add");
+  }
+  const handleChangePage = (event, newPage) => {
+    setPage(newPage);
+  };
+  const handleChangeRowsPerPage = (event) => {
+    setRowsPerPage(parseInt(event.target.value, 10));
+    setPage(0);
+  };
+
+  console.log("books:", books);
+  return (
+    <>
+      <div
+        className={
+          "relative flex flex-col min-w-0 break-words w-full mb-6 shadow-lg rounded " +
+          (color === "light" ? "bg-white" : "bg-blueGray-700 text-white")
+        }
+      >
+        <div className="rounded-t mb-0 px-4 py-3 border-0">
+          <div className="flex flex-wrap items-center">
+            <div className="relative w-full px-4 max-w-full flex-grow flex-1">
+              <h3
+                className={
+                  "font-semibold text-lg " +
+                  (color === "light" ? "text-blueGray-700" : "text-white")
+                }
+              >
+                {title ?? "Card Tables"}
+              </h3>
+              <button
+                className="bg-blueGray-700 active:bg-blueGray-600 text-white font-bold uppercase text-xs px-4 py-2 rounded shadow hover:shadow-md outline-none focus:outline-none mr-1 ease-linear transition-all duration-150"
+                type="button"
+                onClick={() => handleAdd()}
+              >
+                Add
+              </button>
+            </div>
+          </div>
+        </div>
+        <div className="block w-full overflow-x-auto">
+          {/* Projects table */}
+          <table className="items-center w-full bg-transparent border-collapse">
+            <thead>
+              <tr>
+                <th
+                  className={
+                    "px-6 align-middle border border-solid py-3 text-xs uppercase border-l-0 border-r-0 whitespace-nowrap font-semibold text-left " +
+                    (color === "light"
+                      ? "bg-blueGray-50 text-blueGray-500 border-blueGray-100"
+                      : "bg-blueGray-600 text-blueGray-200 border-blueGray-500")
+                  }
+                >
+                  ID
+                </th>
+                <th
+                  className={
+                    "px-6 align-middle border border-solid py-3 text-xs uppercase border-l-0 border-r-0 whitespace-nowrap font-semibold text-left " +
+                    (color === "light"
+                      ? "bg-blueGray-50 text-blueGray-500 border-blueGray-100"
+                      : "bg-blueGray-600 text-blueGray-200 border-blueGray-500")
+                  }
+                >
+                  NAME
+                </th>
+                <th
+                  className={
+                    "px-6 align-middle border border-solid py-3 text-xs uppercase border-l-0 border-r-0 whitespace-nowrap font-semibold text-left " +
+                    (color === "light"
+                      ? "bg-blueGray-50 text-blueGray-500 border-blueGray-100"
+                      : "bg-blueGray-600 text-blueGray-200 border-blueGray-500")
+                  }
+                >
+                  DESCRIPTION
+                </th>
+                <th
+                  className={
+                    "px-6 align-middle border border-solid py-3 text-xs uppercase border-l-0 border-r-0 whitespace-nowrap font-semibold text-left " +
+                    (color === "light"
+                      ? "bg-blueGray-50 text-blueGray-500 border-blueGray-100"
+                      : "bg-blueGray-600 text-blueGray-200 border-blueGray-500")
+                  }
+                >
+                  AUTHOR
+                </th>
+                <th
+                  className={
+                    "px-6 align-middle border border-solid py-3 text-xs uppercase border-l-0 border-r-0 whitespace-nowrap font-semibold text-left " +
+                    (color === "light"
+                      ? "bg-blueGray-50 text-blueGray-500 border-blueGray-100"
+                      : "bg-blueGray-600 text-blueGray-200 border-blueGray-500")
+                  }
+                >
+                  CATEGORY
+                </th>
+                <th
+                  className={
+                    "px-6 align-middle border border-solid py-3 text-xs uppercase border-l-0 border-r-0 whitespace-nowrap font-semibold text-left " +
+                    (color === "light"
+                      ? "bg-blueGray-50 text-blueGray-500 border-blueGray-100"
+                      : "bg-blueGray-600 text-blueGray-200 border-blueGray-500")
+                  }
+                >
+                  VIP
+                </th>
+                <th
+                  className={
+                    "px-6 align-middle border border-solid py-3 text-xs uppercase border-l-0 border-r-0 whitespace-nowrap font-semibold text-left " +
+                    (color === "light"
+                      ? "bg-blueGray-50 text-blueGray-500 border-blueGray-100"
+                      : "bg-blueGray-600 text-blueGray-200 border-blueGray-500")
+                  }
+                >
+                  COST
+                </th>
+                <th
+                  className={
+                    "px-6 align-middle border border-solid py-3 text-xs uppercase border-l-0 border-r-0 whitespace-nowrap font-semibold text-left " +
+                    (color === "light"
+                      ? "bg-blueGray-50 text-blueGray-500 border-blueGray-100"
+                      : "bg-blueGray-600 text-blueGray-200 border-blueGray-500")
+                  }
+                >
+                  THUMBNAIL
+                </th>
+                <th
+                  className={
+                    "px-6 align-middle border border-solid py-3 text-xs uppercase border-l-0 border-r-0 whitespace-nowrap font-semibold text-left " +
+                    (color === "light"
+                      ? "bg-blueGray-50 text-blueGray-500 border-blueGray-100"
+                      : "bg-blueGray-600 text-blueGray-200 border-blueGray-500")
+                  }
+                >
+                  CHANNEL
+                </th>
+                <th
+                  className={
+                    "px-6 align-middle border border-solid py-3 text-xs uppercase border-l-0 border-r-0 whitespace-nowrap font-semibold text-left " +
+                    (color === "light"
+                      ? "bg-blueGray-50 text-blueGray-500 border-blueGray-100"
+                      : "bg-blueGray-600 text-blueGray-200 border-blueGray-500")
+                  }
+                ></th>
+              </tr>
+            </thead>
+            <tbody>
+              {books
+                .slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage)
+                .map((book, key) => (
+                  <tr key={key}>
+                    <th className="border-t-0 px-6 align-middle border-l-0 border-r-0 text-xs whitespace-nowrap p-4">
+                      <span
+                        className={
+                          "ml-3 font-bold " +
+                          +(color === "light"
+                            ? "text-blueGray-600"
+                            : "text-white")
+                        }
+                      >
+                        {book.id}
+                      </span>
+                    </th>
+                    <td className="border-t-0 px-6 align-middle border-l-0 border-r-0 text-xs whitespace-nowrap p-4">
+                      {book.name}
+                    </td>
+                    <td className="border-t-0 px-6 align-middle border-l-0 border-r-0 text-xs whitespace-nowrap p-4">
+                      {book.description}
+                    </td>
+                    <td className="border-t-0 px-6 align-middle border-l-0 border-r-0 text-xs whitespace-nowrap p-4">
+                      {book.author}
+                    </td>
+                    <td className="border-t-0 px-6 align-middle border-l-0 border-r-0 text-xs whitespace-nowrap p-4">
+                      {book.category}
+                    </td>
+                    <td className="border-t-0 px-6 align-middle border-l-0 border-r-0 text-xs whitespace-nowrap p-4">
+                      {
+                        book.isVip ? <Button size="small" variant="contained">VIP</Button> : <Button size="small" variant="outlined">NORMAL</Button>
+                      }
+                    </td>
+                    <td className="border-t-0 px-6 align-middle border-l-0 border-r-0 text-xs whitespace-nowrap p-4">
+                      {book.cost}
+                    </td>
+                    <td className="border-t-0 px-6 align-middle border-l-0 border-r-0 text-xs whitespace-nowrap p-4">
+                        <div className="flex">
+                            <img
+                            src={book.thumbnail}
+                            alt="..."
+                            className="w-20 h-20 border-2 border-blueGray-50 shadow"
+                            ></img>
+                        </div>
+                    </td>
+                    <td className="border-t-0 px-6 align-middle border-l-0 border-r-0 text-xs whitespace-nowrap p-4">
+                      {book.channel}
+                    </td>
+                    <td className="border-t-0 px-6 align-middle border-l-0 border-r-0 text-xs whitespace-nowrap p-4 text-right">
+                        <TableDropdown />
+                    </td>
+                  </tr>
+                ))}
+            </tbody>
+          </table>
+          <TablePagination
+            component="div"
+            count={100}
+            page={page}
+            onPageChange={handleChangePage}
+            rowsPerPage={rowsPerPage}
+            onRowsPerPageChange={handleChangeRowsPerPage}
+          />
+        </div>
+      </div>
+    </>
+  );
+}
+
+BookCardTable.defaultProps = {
+  color: "light",
+};
+
+BookCardTable.propTypes = {
+  color: PropTypes.oneOf(["light", "dark"]),
+};
--- pages/admin/books/index.js
+++ pages/admin/books/index.js
@@ -3,21 +3,77 @@ import React from "react";
 // components
 
 import CardTable from "components/Cards/CardTable.js";
+import BookCardTable from "components/Cards/BookCardTable";
 
 // layout for page
 
 import Admin from "layouts/Admin.js";
+
+const books = [
+  {
+    id: 1,
+    name: "Book 1",
+    description: "Description 1",
+    author: "Author 1",
+    category: "Category 1",
+    isVip: true,
+    cost: "11111111 VNĐ",
+    thumbnail: "/img/book1.jpg",
+    channel: "Channel 1"
+  },
+  {
+    id: 2,
+    name: "Book 2",
+    description: "Description 2",
+    author: "Author 2",
+    category: "Category 2",
+    isVip: false,
+    cost: "22222222 VNĐ",
+    thumbnail: "/img/book1.jpg",
+    channel: "Channel 2"
+  },
+  {
+    id: 3,
+    name: "Book 3",
+    description: "Description 3",
+    author: "Author 3",
+    category: "Category 3",
+    isVip: false,
+    cost: "33333333 VNĐ",
+    thumbnail: "/img/book1.jpg",
+    channel: "Channel 3"
+  },
+  {
+    id: 4,
+    name: "Book 4",
+    description: "Description 4",
+    author: "Author 4",
+    category: "Category 4",
+    isVip: true,
+    cost: "44444444 VNĐ",
+    thumbnail: "/img/book1.jpg",
+    channel: "Channel 4"
+  },
+  {
+    id: 5,
+    name: "Book 5",
+    description: "Description 5",
+    author: "Author 5",
+    category: "Category 5",
+    isVip: true,
+    cost: "55555555 VNĐ",
+    thumbnail: "/img/book1.jpg",
+    channel: "Channel 5"
+  }
+];
 
 export default function Index() {
   return (
     <>
       <div className="flex flex-wrap mt-4">
         <div className="w-full mb-12 px-4">
-          <CardTable title="Books" />
+          <BookCardTable title="Books" books={books} />
         </div>
-        {/* <div className="w-full mb-12 px-4">
-          <CardTable color="dark" title="Books" />
-        </div> */}
       </div>
     </>
   );
