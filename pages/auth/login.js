065d24f1204bfef330aeb4b105e7fb4cf03a6b7c
Tuan Nguyen <ntuan.2502@gmail.com>
2022-04-09 21:10:25 +0700

    add api for admin login

--- pages/auth/login.js
+++ pages/auth/login.js
@@ -1,49 +1,52 @@
-import React, { useState } from "react";
+import { useEffect, useState } from "react";
 import Link from "next/link";
+import axios from "axios";
 import { toast } from "react-toastify";
+import { updateJwt, updateUser } from "../../redux/storeManage";
+import { useDispatch, useSelector } from "react-redux";
 import { validateEmail } from "../../lib/Helper";
-
-// layoutfor page
 
 import Auth from "layouts/Auth.js";
 import { useRouter } from "next/router";
 
-const superAdmin = {
-  email: "admin@voiz.vn",
-  password: "123456",
-};
 export default function Login() {
   const router = useRouter();
+  const dispatch = useDispatch();
+  const { jwt } = useSelector((state) => state.storeManage);
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
-
   const [rememberMe, setRememberMe] = useState(false);
-  const handleChangeEmail = (event) => {
-    setEmail(event.target.value);
-  };
-  const handleChangePassword = (event) => {
-    setPassword(event.target.value);
-  };
-  const handleChangeRememberMe = (event) => {
-    setRememberMe(event.target.checked);
-  };
 
-  function handleLogin() {
-    // giống github, nếu sai ở bất kì lỗi nào thì báo "Incorrect username or password!"
-    if (email && password) {
-      const buildInEmail = superAdmin.email;
-      const buildInPassword = superAdmin.password;
+  useEffect(() => {
+    if (jwt != "null") {
+      router.push("/admin/dashboard");
+    }
+  }, [jwt]);
 
-      if (
-        validateEmail(email) &&
-        email === buildInEmail &&
-        password === buildInPassword
-      ) {
+  async function handleLogin() {
+    if (validateEmail(email) == false || password == "") {
+      toast.error("Invalid email or password");
+      return;
+    }
+
+    const res = await axios.post(
+      `${process.env.NEXT_PUBLIC_API_URL}/auth/login`,
+      {
+        email,
+        password,
+      }
+    );
+
+    if (res.status === 200) {
+      if (res.data.success == true) {
+        const data = res.data.data;
+        dispatch(updateJwt(data.jwt));
+        dispatch(updateUser(data.user));
         router.push("/admin/dashboard");
-        return;
+      } else {
+        toast.error(res.data.message);
       }
     }
-    toast.error("Incorrect username or password!");
   }
 
   return (
@@ -54,32 +57,14 @@ export default function Login() {
             <div className="relative flex flex-col min-w-0 break-words w-full mb-6 shadow-lg rounded-lg bg-blueGray-200 border-0">
               <div className="rounded-t mb-0 px-6 py-6">
                 <div className="text-center mb-3">
-                  <h6 className="text-blueGray-500 text-sm font-bold">
-                    Sign in with
+                  <h6 className="text-blueGray-500 text-2xl font-bold">
+                    Admin Login
                   </h6>
                 </div>
-                <div className="btn-wrapper text-center">
-                  <button
-                    className="bg-white active:bg-blueGray-50 text-blueGray-700 font-normal px-4 py-2 rounded outline-none focus:outline-none mr-2 mb-1 uppercase shadow hover:shadow-md inline-flex items-center font-bold text-xs ease-linear transition-all duration-150"
-                    type="button"
-                  >
-                    <img alt="..." className="w-5 mr-1" src="/img/github.svg" />
-                    Github
-                  </button>
-                  <button
-                    className="bg-white active:bg-blueGray-50 text-blueGray-700 font-normal px-4 py-2 rounded outline-none focus:outline-none mr-1 mb-1 uppercase shadow hover:shadow-md inline-flex items-center font-bold text-xs ease-linear transition-all duration-150"
-                    type="button"
-                  >
-                    <img alt="..." className="w-5 mr-1" src="/img/google.svg" />
-                    Google
-                  </button>
-                </div>
+
                 <hr className="mt-6 border-b-1 border-blueGray-300" />
               </div>
               <div className="flex-auto px-4 lg:px-10 py-10 pt-0">
-                <div className="text-blueGray-400 text-center mb-3 font-bold">
-                  <small>Or sign in with credentials</small>
-                </div>
                 <div>
                   <div className="relative w-full mb-3">
                     <label
@@ -92,7 +77,7 @@ export default function Login() {
                       type="email"
                       className="border-0 px-3 py-3 placeholder-blueGray-300 text-blueGray-600 bg-white rounded text-sm shadow focus:outline-none focus:ring w-full ease-linear transition-all duration-150"
                       placeholder="Email: admin@voiz.vn"
-                      onChange={handleChangeEmail}
+                      onChange={(e) => setEmail(e.target.value)}
                       value={email}
                     />
                   </div>
@@ -108,7 +93,7 @@ export default function Login() {
                       type="password"
                       className="border-0 px-3 py-3 placeholder-blueGray-300 text-blueGray-600 bg-white rounded text-sm shadow focus:outline-none focus:ring w-full ease-linear transition-all duration-150"
                       placeholder="Password: 123456"
-                      onChange={handleChangePassword}
+                      onChange={(e) => setPassword(e.target.value)}
                       value={password}
                     />
                   </div>
@@ -118,7 +103,7 @@ export default function Login() {
                         id="customCheckLogin"
                         type="checkbox"
                         className="form-checkbox border-0 rounded text-blueGray-700 ml-1 w-5 h-5 ease-linear transition-all duration-150"
-                        onChange={handleChangeRememberMe}
+                        onChange={() => setRememberMe(!rememberMe)}
                         value={rememberMe}
                       />
                       <span className="ml-2 text-sm font-semibold text-blueGray-600">
@@ -133,28 +118,10 @@ export default function Login() {
                       type="button"
                       onClick={() => handleLogin()}
                     >
-                      Sign In
+                      Login
                     </button>
                   </div>
                 </div>
-              </div>
-            </div>
-            <div className="flex flex-wrap mt-6 relative">
-              <div className="w-1/2">
-                <a
-                  href="#pablo"
-                  onClick={(e) => e.preventDefault()}
-                  className="text-blueGray-200"
-                >
-                  <small>Forgot password?</small>
-                </a>
-              </div>
-              <div className="w-1/2 text-right">
-                <Link href="/auth/register">
-                  <a href="#pablo" className="text-blueGray-200">
-                    <small>Create new account</small>
-                  </a>
-                </Link>
               </div>
             </div>
           </div>
